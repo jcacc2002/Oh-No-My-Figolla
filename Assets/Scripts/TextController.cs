029767e34ba39f428973ca8f0e2e7e1a9763e511
jcacc2002 <122575451+jcacc2002@users.noreply.github.com>
2023-12-12 12:55:48 +0100

    Screen Addition
    + Created Canvas, Panel, Text
    + Created TextController script
    
    The randomised text cycles between 3 sentences so I still need to randomise the values within.
    
    Still need to find a way to connect everything together.
    
    Thx bai <3

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Figolla
+{
+    public class TextController : MonoBehaviour
+    {
+        public TextMeshProUGUI screenText;
+        public int timer;
+
+        private List<string> sentences = new List<string>()
+        {
+            "Dial clockwise, 4.",
+            "Switch on.",
+            "Button press 6 times."
+        };
+
+        private string lastInstruction = string.Empty;
+
+            void Start()
+        {
+            screenText = GetComponent<TextMeshProUGUI>();
+            screenText.text = "FEED ME!";
+            
+            StartCoroutine(ChangeTextRoutine());
+            
+        }
+            
+            private IEnumerator ChangeTextRoutine()
+            {
+                while (true)
+                {
+                    yield return new WaitForSeconds(timer);
+                    int rnd = Random.Range(0, sentences.Count);
+                    string randomSentence = sentences[rnd];
+                    sentences.RemoveAt(rnd);
+                    screenText.text = randomSentence;
+                    
+                    if (lastInstruction != string.Empty)
+                    {
+                        sentences.Add(lastInstruction);
+                    }
+
+                    lastInstruction = randomSentence;
+                }
+            }
+        
+    }
+}
